e6e8f0c6dc9df7e303166893457bda32be653fcd
Shane Nguyen <sn103103@gmail.com>
2022-12-30 05:06:28 -0800

    update anchorscroller.js to support id's that start with digits

--- assets/js/anchorscroller.js
+++ assets/js/anchorscroller.js
@@ -1,15 +1,16 @@
 // Original JavaScript code by Chirp Internet: chirpinternet.eu
 // Please acknowledge use of this code by including this header.
 
-document.querySelectorAll("a").forEach(function(current) {
-    if(!current.hash) return;
-    if(current.origin + current.pathname != self.location.href) return;
+window.addEventListener("DOMContentLoaded", function(e) {
+  var links = document.getElementsByTagName("A");
+  for(var i=0; i < links.length; i++) {
+    if(!links[i].hash) continue;
+    if(links[i].origin + links[i].pathname != self.location.href) continue;
     (function(anchorPoint) {
-      if(anchorPoint) {
-        current.addEventListener("click", function(e) {
-          anchorPoint.scrollIntoView({behavior: "smooth"});
-          e.preventDefault();
-        }, false);
-      }
-    })(document.querySelector(current.hash));
-  });
+      links[i].addEventListener("click", function(e) {
+        if(anchorPoint) anchorPoint.scrollIntoView({behavior: "smooth"});
+        e.preventDefault();
+      }, false);
+    })(document.getElementById(links[i].hash.replace(/#/, "")));
+  }
+}, false);
